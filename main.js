9ef97ed78209542d6d80ac78aa544440abc454d3
Micayla Presley <preslemi@oregonstate.edu>
2019-11-25 15:01:11 -0800

    add js
    "add" Function

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-const BASE_URL = "https://zagster-service.herokuapp.com"
+// const BASE_URL = "https://zagster-service.herokuapp.com"
 
-$(updateView)
+// $(updateView)
 
-function updateView() {
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+// function updateView() {
+//   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+// }
+
+// function updateRideCount(data) {
+//   numberOfRides = data.count
+//   $("h2#rideCount").html(numberOfRides)
+// }
+// // a memory location thatnever changes
+// const BASE_URL = "https://zagster-service.herokuapp.com"
+// const PI = 3.14159
+
+//jQuery command. Wait until web page loads call function
+//whose name is in parentheses
+//function call means run the code
+//$(greeter)
+
+//call function add (use it - make it run)
+add(2,3);
+//greeter is the function identifier. name is the functions
+//argument (info it need to do its job)
+function add(num1, num2){
+    answer = num1+num2;
+    console.log("The answer is: " + answer);
+    return answer;
 }
+// function greeter(name){
+//     alert("welcome to" + name + " data visualization")
+// }
 
-function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h2#rideCount").html(numberOfRides)
-}
+// greeter("Micayla Presley's")
