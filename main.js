a20fedf84195b5a69dfb0f67aa991648ea3c80f6
Micayla Presley <preslemi@oregonstate.edu>
2019-11-25 15:06:16 -0800

    Correcting Zagster count function

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// const BASE_URL = "https://zagster-service.herokuapp.com"
+ const BASE_URL = "https://zagster-service.herokuapp.com"
 
-// $(updateView)
+ $(updateView)
 
-// function updateView() {
-//   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-// }
+ function updateView() {
+   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+ }
 
 // function updateRideCount(data) {
 //   numberOfRides = data.count
