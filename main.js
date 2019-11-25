dcc5c5dab67852a71862c07352eb67638b43ea1b
Micayla Presley <preslemi@oregonstate.edu>
2019-11-25 15:50:55 -0800

    Exercise 16

--- main.js
+++ main.js
@@ -74,3 +74,24 @@ function updateView() {
     ).then(updateChart);
 }
 
+function perYear(data) {
+
+    // [{"9":220},{"10:141"},{"11":89},{"12":16}]
+    for (var index = 0, month=9; index <= 3, month<=12; ++index, ++month){
+        months2016.push(data[2016] [index] [month]);
+    }
+
+    console.log("2016 data by months is easy" + months2016)
+
+
+    for (var index = 0, month = 1; index <=11, month <=12, ++index, ++month) {
+        months2017.push(data[2017][index][month]);
+    }
+
+    console.log("2017 data by months is easy" + months2017)
+
+    for (var index = 0, month = 1; index <=11, month <=12, ++index, ++month) {
+        months2018.push(data[2018][index][month]);
+    }
+
+    console.log("2018 data by months is easy" + months2018)
